27f14801067aaccd9566e0d15fd69d68caf2437f
vikto-a <victor.nain.eu@gmail.com>
2022-10-02 13:36:09 -0400

    basic navbar styles

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,10 +2,10 @@ import Link from 'next/link';
 
 export const Navbar: React.FC = () => {
 	return (
-		<nav>
-			<span>Murph</span>
+		<nav className="sticky top-6 mx-6 flex items-center justify-between rounded-md bg-neutral-900 p-3">
+			<span className="font-bold uppercase">Murph</span>
 
-			<div>
+			<div className="flex items-center gap-6">
 				<Link href="/">Home</Link>
 				<Link href="/leaderboard">Leaderboard</Link>
 				<Link href="/login">Login</Link>
