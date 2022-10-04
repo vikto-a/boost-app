5c1b6f9ad5b49245e1bb5a4250cadf6f26e382c7
vikto-a <victor.nain.eu@gmail.com>
2022-10-04 09:01:57 -0400

    installed luxon and types

--- src/pages/workout.tsx
+++ src/pages/workout.tsx
@@ -5,6 +5,14 @@ import { trpc } from '../utils/trpc';
 const Workout: NextPage = () => {
 	const hello = trpc.useQuery(['example.hello', { text: 'from tRPC' }]);
 
+	/*
+	
+	- get datetime
+	- calc time since date
+	- display time to screen
+	
+	*/
+
 	return (
 		<Page title="Murph Workout">
 			<h1>{hello.data ? hello.data.greeting : 'loading...'}</h1>
