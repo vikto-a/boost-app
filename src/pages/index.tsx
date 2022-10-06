91d3a5fac7cfd06a1f8a27c3b83dd793eaebb7ec
vikto-a <victor.nain.eu@gmail.com>
2022-10-05 21:36:50 -0400

    remove trpc query in home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,11 @@
 import { Hero } from '@components';
 import type { NextPage } from 'next';
 import { Page } from '@layouts';
-import { trpc } from '../utils/trpc';
 
 const Home: NextPage = () => {
-	const hello = trpc.useQuery(['example.hello', { text: 'from tRPC' }]);
-
 	return (
 		<Page title="Murph">
 			<Hero />
-			{/* <h1>{hello.data ? hello.data.greeting : 'loading...'}</h1> */}
 		</Page>
 	);
 };
