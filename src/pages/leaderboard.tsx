a430954085042fe3f1cddcd11552a641b64e981a
vikto-a <victor.nain.eu@gmail.com>
2022-10-05 21:21:53 -0400

    coming soon leaderboard

--- src/pages/leaderboard.tsx
+++ src/pages/leaderboard.tsx
@@ -1,19 +1,15 @@
 import { Center, Page } from '@layouts';
 
 import type { NextPage } from 'next';
-import { trpc } from '../utils/trpc';
 
-const Home: NextPage = () => {
-	const hello = trpc.useQuery(['example.hello', { text: 'from tRPC' }]);
-
+const Leaderboard: NextPage = () => {
 	return (
 		<Page title="Murph Leaderboard">
 			<Center>
-				<h1 className="text-2xl font-bold">Leaderboard</h1>
+				<h1 className="text-2xl font-bold">Coming Soon</h1>
 			</Center>
-			{/* <h1>{hello.data ? hello.data.greeting : 'loading...'}</h1> */}
 		</Page>
 	);
 };
 
-export default Home;
+export default Leaderboard;
