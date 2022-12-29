8afb8aff8b1b4811803918c37ea25e9877d40220
vikto-a <victor.nain.eu@gmail.com>
2022-12-28 22:43:53 -0500

    add empty text

--- src/pages/leaderboard.tsx
+++ src/pages/leaderboard.tsx
@@ -1,59 +1,64 @@
-import { Center, Page } from '@layouts';
-import { LeaderboardCard, Loading } from '@components';
-import { useEffect, useState } from 'react';
+import { Center, Page } from "@layouts";
+import { Card, LeaderboardCard, Loading } from "@components";
+import { useEffect, useState } from "react";
 
-import { DateTime } from 'luxon';
-import { MurphWithUser } from '@type';
-import type { NextPage } from 'next';
-import { trpc } from 'utils/trpc';
+import { DateTime } from "luxon";
+import { MurphWithUser } from "@type";
+import type { NextPage } from "next";
+import { trpc } from "utils/trpc";
 
 const Leaderboard: NextPage = () => {
-	const [top, setTop] = useState<
-		{
-			elapsed: number;
-			murph: MurphWithUser;
-		}[]
-	>([]);
+  const [top, setTop] = useState<
+    {
+      elapsed: number;
+      murph: MurphWithUser;
+    }[]
+  >([]);
 
-	const leader = trpc.useQuery(['leaderboard.getMonth']);
-	console.log(typeof leader.data);
+  const leader = trpc.useQuery(["leaderboard.getMonth"]);
 
-	useEffect(() => {
-		if (leader.data) {
-			const durations = leader.data.map((run) => {
-				const start = DateTime.fromJSDate(run.start);
-				const end = DateTime.fromJSDate(run.lastSprintEndTime);
-				const diff = end?.diff(start);
+  useEffect(() => {
+    if (leader.data) {
+      const durations = leader.data.map((run) => {
+        const start = DateTime.fromJSDate(run.start);
+        const end = DateTime.fromJSDate(run.lastSprintEndTime);
+        const diff = end?.diff(start);
 
-				return { elapsed: diff.toMillis(), murph: run };
-			});
+        return { elapsed: diff.toMillis(), murph: run };
+      });
 
-			const sorted = durations.sort((a, b) => a.elapsed - b.elapsed);
+      const sorted = durations.sort((a, b) => a.elapsed - b.elapsed);
 
-			const topTen = sorted.slice(0, 10);
+      const topTen = sorted.slice(0, 10);
 
-			setTop(topTen);
-		}
-	}, [leader.data]);
+      setTop(topTen);
+    }
+  }, [leader.data]);
 
-	return (
-		<Page title="Murph Leaderboard">
-			<Center>
-				<h1 className="text-2xl font-bold">
-					{DateTime.now().toFormat('MMMM')} Leaderboard
-				</h1>
-				{leader.data ? (
-					<div className="grid w-full gap-6">
-						{top.map((item, i) => (
-							<LeaderboardCard murph={item.murph} rank={i + 1} key={i} />
-						))}
-					</div>
-				) : (
-					<Loading />
-				)}
-			</Center>
-		</Page>
-	);
+  return (
+    <Page title="Murph Leaderboard">
+      <Center>
+        <h1 className="text-2xl font-bold">
+          {DateTime.now().toFormat("MMMM")} Leaderboard
+        </h1>
+        {leader.isLoading ? (
+          <Loading />
+        ) : (
+          <>
+            {leader.data?.length ? (
+              <div className="grid w-full gap-6">
+                {top.map((item, i) => (
+                  <LeaderboardCard murph={item.murph} rank={i + 1} key={i} />
+                ))}
+              </div>
+            ) : (
+              <Card color="red">No Murphs this month.</Card>
+            )}
+          </>
+        )}
+      </Center>
+    </Page>
+  );
 };
 
 export default Leaderboard;
